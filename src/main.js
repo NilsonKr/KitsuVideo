f7d2f6509fc382c64d18716eddc6cfb1e687ff78
NilsonKr <nuotrok444@gmail.com>
2021-02-20 18:16:26 -0500

    Done the aside menu and burguer menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './components/header.js'
+
+
+const burguerButton = document.querySelector('#burguer')
+const menu = document.querySelector('#Menu')
+
+document.body.addEventListener('click', ev => {
+    const target = ev.target
+    if(target.classList.contains('burguer-click')){
+        burguerButton.classList.toggle('active-burguer')
+        menu.classList.toggle('menuActive')
+    }
+})
